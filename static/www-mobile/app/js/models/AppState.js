5eb380db08a683daa24619e9afd062c66265fdf5
phisch124 <philippe.schwaller@rega-sense.ch>
2013-07-14 13:39:53 +0200

    AppState

---
+++ static/www-mobile/app/js/models/AppState.js
@@ -0,0 +1,55 @@
+// AppState
+// ==============
+
+// Includes file dependencies
+define([ "jquery", "../models/PartyModel", "backbone", ], function( $, PartyModel ) {
+
+    // The Model constructor
+    var Model = Backbone.Model.extend( {
+        urlRoot: 'api/appstate',
+        idAttribute: "_id",
+        defaults: {
+            party: ((Math.random() > 0.5) ? null : new PartyModel({name: "testparty", playlist: []}))
+        },
+
+        joinPartyByName: function(name, callback){ 
+            console.log("getByName");
+            // Fetches party id from it's name
+                $.getJSON('api/party/joinPartyByName/'+ encodeURIComponent(name))
+
+                // In case of ajax success :
+                .success($.proxy(function(data){
+                    
+                    // The server may still have encountered an internal error (typically "no party with such name")
+                    if(data.error){
+                        callback(data.error);
+                    } 
+                    // Otherwise, we can process the model
+                    else {
+                        var party = new Model(data);
+
+                        this.set("party", party);
+                        callback(null);
+                    }
+                }, this))
+                
+                // In case of error :
+                .error(function(data){
+                    callback("Sth went wrong...");
+                });
+            },
+
+            fetch = function(opts) {
+                var mod = this;
+
+                setTimeout(function(){
+                    opts.success(mod);
+                }, 1000);
+            }
+
+    } );
+
+    return Model;
+
+} );
+
