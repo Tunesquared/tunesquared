263505a4cc2ac87c63fdc1482735506f2104b9ea
Agent-H <hadrien.milano@gmail.com>
2013-09-21 14:00:33 +0200

    updated router

--- static/www/js/mixins/Router.js
+++ static/www/js/mixins/Router.js
@@ -1,5 +1,28 @@
 'use strict';
 
+/* Mixin for implementing a backbone based router in a react component.
+
+	example:
+	React.createClass({
+		mixins: [Router],
+		routes: {
+			'search/:q': function (q) {
+				this.setState({
+					page: 'search',
+					searchQuery: q
+				});
+			},
+			'': function () {
+				this.setState({
+					page: 'home'
+				});
+			}
+		}
+		...
+
+		note: you still need to start Backbone's history somewhere
+ */
+
 define(['backbone', 'underscore'], function(Backbone, _){
 	var Router = {
 		componentDidMount: function(){
@@ -7,7 +30,7 @@ define(['backbone', 'underscore'], function(Backbone, _){
 				this.routes[i] = _.bind(this.routes[i], this);
 			}
 
-			new (Backbone.Router.extend({
+			this.router = new (Backbone.Router.extend({
 				routes: this.routes
 			}))();
 		}
