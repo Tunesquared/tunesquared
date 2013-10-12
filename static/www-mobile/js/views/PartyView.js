afcc6b216960019cf48bf7c9246a5a820d6c234b
Hadrien Milano <hadrien.milano@gmail.com>
2013-10-12 23:15:35 +0200

    added song templates and fixes

--- static/www-mobile/js/views/PartyView.js
+++ static/www-mobile/js/views/PartyView.js
@@ -14,4 +14,14 @@ define(['$', 'bs/collapse'], function($) {
   $('.side-menu').click(function(){
     menu.removeClass('active');
   });
+
+  $('.vote-buttons button').click(function(){
+    if ($(this).hasClass('btn-success')) {
+      $(this).parent().children('.btn-info').removeClass('btn-info').removeAttr('disabled').addClass('btn-danger');
+      $(this).removeClass('btn-success').addClass('btn-info').attr('disabled', 'disabled');
+    } else if ($(this).hasClass('btn-danger')) {
+      $(this).parent().children('.btn-info').removeClass('btn-info').removeAttr('disabled').addClass('btn-success');
+      $(this).removeClass('btn-danger').addClass('btn-info').attr('disabled', 'disabled');
+    }
+  });
 });
